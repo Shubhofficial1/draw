7fe1b4bd179068bb5d2b1bcd8867590af78b83c4
shubham kumar <shubhofficial1@gmail.com>
2024-05-17 02:32:14 +0530

    Added Routing Configuration

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,24 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store/store.js";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" index={true} element={<HomeScreen />}></Route>
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
---
+++ frontend/src/screens/HomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Whiteboard from "../components/Whiteboard";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <Whiteboard />
+    </>
+  );
+};
+
+export default HomeScreen;
