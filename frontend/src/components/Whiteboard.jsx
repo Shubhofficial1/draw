ded63332c775b44fc0b500549495e699c126c762
shubham kumar <shubhofficial1@gmail.com>
2024-05-18 22:37:50 +0530

    Added Canvas & drawn simple elements

--- frontend/src/components/Whiteboard.jsx
+++ frontend/src/components/Whiteboard.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useRef, useLayoutEffect } from "react";
 import Menu from "./Menu";
+import rough from "roughjs";
 
 const Whiteboard = () => {
+  const canvasRef = useRef();
+
+  useLayoutEffect(() => {
+    const canvas = canvasRef.current;
+    const rc = rough.canvas(canvas);
+    rc.rectangle(10, 10, 100, 100);
+    rc.rectangle(20, 20, 300, 300);
+    rc.line(80, 120, 300, 100);
+  }, []);
+
   return (
-    <div>
+    <>
       <Menu />
-    </div>
+      <canvas
+        ref={canvasRef}
+        width={window.innerWidth}
+        height={window.innerHeight}
+      />
+    </>
   );
 };
 
