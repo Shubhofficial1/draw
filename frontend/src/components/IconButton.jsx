42c459ea41ff267d596b5cd408d6c96050647fbe
shubham kumar <shubhofficial1@gmail.com>
2024-05-18 17:33:31 +0530

    Connected Action to Rectangle button

--- frontend/src/components/IconButton.jsx
+++ frontend/src/components/IconButton.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setToolType } from "../redux/slices/whiteboardSlice";
 
 const IconButton = ({ src, type }) => {
+  const dispatch = useDispatch();
+  const selectedToolType = useSelector((state) => state.whiteboard.tool);
+
+  const setToolTypeHandler = () => {
+    dispatch(setToolType(type));
+  };
+
   return (
-    <button className="w-[40px] h-[40px] rounded-md border-none bg-emerald-100 hover:bg-emerald-300 flex items-center justify-center ">
+    <button
+      className={`w-[40px] h-[40px] rounded-md border-none bg-emerald-100 hover:bg-emerald-300 flex items-center justify-center ${
+        selectedToolType == type ? "bg-emerald-300" : ""
+      }`}
+      onClick={setToolTypeHandler}
+    >
       <img src={src} type={type} className="w-[80%] h-[80%]" />
     </button>
   );
--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice.js";
+import whiteboardSliceReducer from "../slices/whiteboardSlice.js";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    whiteboard: whiteboardSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
