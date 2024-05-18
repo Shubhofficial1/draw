d037c0a8571b85048075026c45a32b61e163c2f4
shubham kumar <shubhofficial1@gmail.com>
2024-05-18 16:54:49 +0530

    Added Menu Component

---
+++ frontend/src/components/Menu.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div className="absolute top-0 right-[calc(50%-175px)] h-[60px] w-[350px] bg-slate-200  rounded-b-md flex items-center justify-around"></div>
+  );
+};
+
+export default Menu;
--- frontend/src/components/Whiteboard.jsx
+++ frontend/src/components/Whiteboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Menu from "./Menu";
 
 const Whiteboard = () => {
-  return <div>Whiteboard</div>;
+  return (
+    <div>
+      <Menu />
+    </div>
+  );
 };
 
 export default Whiteboard;
