3b23417cf9aaeecfaf32b24292a98d8a9ca314ad
shubham kumar <shubhofficial1@gmail.com>
2024-09-19 22:03:50 +0530

    Configured Cursor Slice

--- frontend/src/redux/store/store.js
+++ frontend/src/redux/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "../slices/apiSlice.js";
 import whiteboardSliceReducer from "../slices/whiteboardSlice.js";
+import cursorSliceReducer from "../slices/cursorSlice.js";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     whiteboard: whiteboardSliceReducer,
+    cursor: cursorSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
